ca85fa78b1f1254addfbeec88e16713f75704d6b
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2022-12-21 15:09:29 +0000

    Turn numbers clickable and displayable

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Functions for all the basic math operators.
+
 const add = function(a, b) {
 
   return a + b;	
@@ -20,6 +22,8 @@ const divide = function(a, b) {
 
 };
 
+// Function operate.
+
 const operate = function(operation, a, b) {
 
   if(operation == "+") return add(a, b);
@@ -27,3 +31,18 @@ const operate = function(operation, a, b) {
   else if(operation == "*") return multiply(a, b);
   else if(operation == "/") return divide(a, b);
 }
+
+
+// Function that populates the display when number buttons are clicked.
+
+let stored = "";
+const display = document.querySelector("#display");
+const digits = document.querySelectorAll(".digit");
+
+digits.forEach(digit => {
+
+  digit.addEventListener("click", () => {
+    stored += digit.textContent;
+    return display.textContent = stored;
+  });
+});
